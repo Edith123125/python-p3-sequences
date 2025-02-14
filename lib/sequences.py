99e9ac282fdb8f70b053456f92fb9b7db85ae631
Edith Gatwiri <edithgatwiri70@gmail.com>
2025-02-14 16:53:03 +0300

    sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+
+    # Initialize Fibonacci sequence  [0, 1]
+    fibonacci_sequence = [0, 1]
+
+    for i in range(2, length):
+        next_fib = fibonacci_sequence[-1] + fibonacci_sequence[-2] 
+        fibonacci_sequence.append(next_fib)
+
+    print(fibonacci_sequence[:length])
+    
+    
+
